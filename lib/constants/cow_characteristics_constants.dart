6b8246b496172cac7a014f25c78c74d6123f8d40
Somtochi Umeh <74836691+SomtochiUmeh@users.noreply.github.com>
2024-07-25 19:43:37 -0700

    Adding dropdowns for cow characteristics

---
+++ lib/constants/cow_characteristics_constants.dart
@@ -0,0 +1,90 @@
+class CowCharacteristicsConstants {
+  static const List<String> liveWeightOptions = [
+    'Choose live weight (kg)',
+    '100',
+    '125',
+    '150',
+    '175',
+    '200',
+    '225',
+    '250',
+    '275',
+    '300',
+    '325',
+    '350',
+    '375',
+    '400',
+    '425',
+    '450',
+    '475',
+    '500',
+    '525',
+    '550',
+    '575',
+    '600'
+  ];
+
+  static const List<String> pregnancyOptions = [
+    'Choose pregnancy month',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
+  ];
+
+  static const List<String> milkFatOptions = [
+    'Choose milk fat %',
+    '3.0',
+    '3.1',
+    '3.2',
+    '3.3',
+    '3.4',
+    '3.5',
+    '3.6',
+    '3.7',
+    '3.8',
+    '3.9',
+    '4.0',
+    '4.1',
+    '4.2',
+    '4.3',
+    '4.4',
+    '4.5',
+    '4.6',
+    '4.7',
+    '4.8',
+    '4.9',
+    '5.0',
+    '5.1',
+    '5.2'
+  ];
+
+  static const List<String> milkProteinOptions = [
+    'Choose milk protein %',
+    '2.6',
+    '2.7',
+    '2.8',
+    '2.9',
+    '3.0',
+    '3.1',
+    '3.2',
+    '3.3',
+    '3.4',
+    '3.5',
+    '3.6'
+  ];
+
+  static const List<String> lactationOptions = [
+    'Choose lactation stage',
+    'Dry',
+    'Early lactation',
+    'Mid lactation',
+    'Late lactation'
+  ];
+}
