c8727412dba2f8143adb715ef71fd4454ee34485
Somtochi Umeh <74836691+SomtochiUmeh@users.noreply.github.com>
2025-04-05 20:06:19 -0700

    adding demo links to README and updating `intl_ar.arb` file

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -20,101 +20,261 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ar';
 
-  static String m0(type) => "تمت إضافة جميع خيارات ${type}.";
+  static String m0(type) => "تم إضافة جميع خيارات ${type}.";
 
-  static String m1(value) => "${value} كجم/يوم";
+  static String m1(value) => "${value} كغم/يوم";
 
-  static String m2(value) => "${value} كجم";
+  static String m2(value) => "${value} كغم";
 
-  static String m3(value) => "${value} ميجاجول/يوم";
+  static String m3(value) => "${value} ميغاجول/يوم";
 
-  static String m4(value) => "${value}٪";
+  static String m4(value) => "${value}%";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "add": MessageLookupByLibrary.simpleMessage("إضافة"),
-        "addConcentrate": MessageLookupByLibrary.simpleMessage("إضافة مركّز"),
-        "addFodder": MessageLookupByLibrary.simpleMessage("إضافة علف"),
-        "allOptionsHaveBeenAdded": m0,
-        "caIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول الكالسيوم"),
-        "caIntakeLabelWithUnit":
-            MessageLookupByLibrary.simpleMessage("تناول الكالسيوم\n(٪)"),
-        "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
-        "chooseConcentrate":
-            MessageLookupByLibrary.simpleMessage("اختيار المركّز"),
-        "chooseFodder": MessageLookupByLibrary.simpleMessage("اختيار العلف"),
-        "concentrate": MessageLookupByLibrary.simpleMessage("مركّز"),
-        "concentrateIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول المركّز"),
-        "concentrateIntakeLabelWithUnit":
-            MessageLookupByLibrary.simpleMessage("تناول المركّز\n(٪)"),
-        "cowCharacteristics":
-            MessageLookupByLibrary.simpleMessage("خصائص البقرة"),
-        "cowRequirements":
-            MessageLookupByLibrary.simpleMessage("متطلبات البقرة"),
-        "cpIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول البروتين الخام"),
-        "cpIntakeLabelWithUnit":
-            MessageLookupByLibrary.simpleMessage("تناول البروتين الخام\n(٪)"),
-        "crudeProteinLabel":
-            MessageLookupByLibrary.simpleMessage("البروتين الخام"),
-        "dmIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول المادة الجافة"),
-        "dmIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
-            "تناول المادة الجافة\n(كجم/يوم)"),
-        "dryCowFeeding":
-            MessageLookupByLibrary.simpleMessage("تغذية البقرة الجافة"),
-        "dryCowPeriod": MessageLookupByLibrary.simpleMessage(
-            "موعد الولادة خلال 45 إلى 60 يوم"),
-        "dryMatterIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول المادة الجافة"),
-        "earlyLactationFeeding":
-            MessageLookupByLibrary.simpleMessage("تغذية مرحلة الإدرار المبكرة"),
-        "earlyLactationPeriod":
-            MessageLookupByLibrary.simpleMessage("الإدرار المبكر (14-100 يوم)"),
-        "feedFormula": MessageLookupByLibrary.simpleMessage("تركيبة العلف"),
-        "feedingGuidelines":
-            MessageLookupByLibrary.simpleMessage("إرشادات التغذية"),
-        "fodder": MessageLookupByLibrary.simpleMessage("علف"),
-        "freshFeedIntake": MessageLookupByLibrary.simpleMessage(
-            "تناول العلف الطازج (كجم/يوم)"),
-        "kgPerDay": m1,
-        "kgValue": m2,
-        "lateLactationFeeding": MessageLookupByLibrary.simpleMessage(
-            "تغذية مرحلة الإدرار المتأخرة"),
-        "lateLactationPeriod": MessageLookupByLibrary.simpleMessage(
-            "الإدرار المتأخر (أكثر من 200 يوم)"),
-        "meIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول الطاقة الأيضية"),
-        "meIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
-            "تناول الطاقة الأيضية\n(ميجاجول/يوم)"),
-        "midLactationFeeding": MessageLookupByLibrary.simpleMessage(
-            "تغذية مرحلة الإدرار المتوسطة"),
-        "midLactationPeriod": MessageLookupByLibrary.simpleMessage(
-            "الإدرار المتوسط (100 إلى 200 يوم)"),
-        "milkYield": MessageLookupByLibrary.simpleMessage("إنتاج الحليب"),
-        "mjPerDayValue": m3,
-        "ndfIntakeLabel":
-            MessageLookupByLibrary.simpleMessage("تناول الألياف المتعادلة"),
-        "ndfIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
-            "تناول الألياف المتعادلة\n(٪)"),
-        "ndfLabel": MessageLookupByLibrary.simpleMessage("الألياف المتعادلة"),
-        "pIntakeLabel": MessageLookupByLibrary.simpleMessage("تناول الفوسفور"),
-        "pIntakeLabelWithUnit":
-            MessageLookupByLibrary.simpleMessage("تناول الفوسفور\n(٪)"),
-        "percentageValue": m4,
-        "period": MessageLookupByLibrary.simpleMessage("الفترة"),
-        "planFeed": MessageLookupByLibrary.simpleMessage("تخطيط التغذية"),
-        "pleaseAllFields":
-            MessageLookupByLibrary.simpleMessage("يرجى ملء جميع الحقول."),
-        "pleaseFillInAllFieldsBeforeViewingRequirements":
-            MessageLookupByLibrary.simpleMessage(
-                "يرجى ملء جميع الحقول قبل عرض المتطلبات."),
-        "selectRationIngredient":
-            MessageLookupByLibrary.simpleMessage("اختيار مكون العليقة"),
-        "totals": MessageLookupByLibrary.simpleMessage("المجاميع"),
-        "viewCowRequirements":
-            MessageLookupByLibrary.simpleMessage("عرض متطلبات البقرة")
-      };
+    "add": MessageLookupByLibrary.simpleMessage("إضافة"),
+    "addConcentrate": MessageLookupByLibrary.simpleMessage("إضافة مركز"),
+    "addFodder": MessageLookupByLibrary.simpleMessage("إضافة علف"),
+    "allOptionsHaveBeenAdded": m0,
+    "caIntakeLabel": MessageLookupByLibrary.simpleMessage("استهلاك الكالسيوم"),
+    "caIntakeLabelKgPerD": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الكالسيوم\n(كغم/يوم)",
+    ),
+    "caIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الكالسيوم\n(%)",
+    ),
+    "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
+    "choose": MessageLookupByLibrary.simpleMessage("اختر"),
+    "chooseConcentrate": MessageLookupByLibrary.simpleMessage("اختر المركز"),
+    "chooseFodder": MessageLookupByLibrary.simpleMessage("اختر العلف"),
+    "chooseLactationStage": MessageLookupByLibrary.simpleMessage(
+      "اختر مرحلة الإدرار",
+    ),
+    "chooseLiveWeightKg": MessageLookupByLibrary.simpleMessage(
+      "اختر الوزن الحي (كغم)",
+    ),
+    "chooseMilkFat": MessageLookupByLibrary.simpleMessage(
+      "اختر نسبة دهون الحليب %",
+    ),
+    "chooseMilkProtein": MessageLookupByLibrary.simpleMessage(
+      "اختر نسبة بروتين الحليب %",
+    ),
+    "choosePregnancyMonth": MessageLookupByLibrary.simpleMessage(
+      "اختر شهر الحمل",
+    ),
+    "concentrate": MessageLookupByLibrary.simpleMessage("مركز"),
+    "concentrateBrewersByProductFresh": MessageLookupByLibrary.simpleMessage(
+      "منتجات البيرة الثانوية الطازجة",
+    ),
+    "concentrateChickpeaMeal": MessageLookupByLibrary.simpleMessage(
+      "وجبة الحمص",
+    ),
+    "concentrateDryBread": MessageLookupByLibrary.simpleMessage("خبز جاف"),
+    "concentrateGroundnutMeal": MessageLookupByLibrary.simpleMessage(
+      "وجبة الفول السوداني",
+    ),
+    "concentrateIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك المركزات",
+    ),
+    "concentrateIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك المركزات\n(%)",
+    ),
+    "concentrateMaizeBran": MessageLookupByLibrary.simpleMessage("نخالة الذرة"),
+    "concentrateMaizeGrain": MessageLookupByLibrary.simpleMessage("حبوب الذرة"),
+    "concentrateMineralLick": MessageLookupByLibrary.simpleMessage(
+      "ملعقة المعادن",
+    ),
+    "concentrateMolasses": MessageLookupByLibrary.simpleMessage("دبس السكر"),
+    "concentratePearlMilletGrain": MessageLookupByLibrary.simpleMessage(
+      "حبوب الدخن اللؤلؤي",
+    ),
+    "concentrateSalt": MessageLookupByLibrary.simpleMessage("ملح"),
+    "concentrateSesameSeedMeal": MessageLookupByLibrary.simpleMessage(
+      "وجبة بذور السمسم",
+    ),
+    "concentrateSorghumBrewersGrainFresh": MessageLookupByLibrary.simpleMessage(
+      "حبوب الذرة الرفيعة من منتجات البيرة الطازجة",
+    ),
+    "concentrateSorghumGrain": MessageLookupByLibrary.simpleMessage(
+      "حبوب الذرة الرفيعة",
+    ),
+    "concentrateSoybeanCake": MessageLookupByLibrary.simpleMessage(
+      "كعكة فول الصويا",
+    ),
+    "concentrateSoybeanHulls": MessageLookupByLibrary.simpleMessage(
+      "قشور فول الصويا",
+    ),
+    "concentrateSoybeanMeal": MessageLookupByLibrary.simpleMessage(
+      "وجبة فول الصويا",
+    ),
+    "concentrateSoybeanOil": MessageLookupByLibrary.simpleMessage(
+      "زيت فول الصويا",
+    ),
+    "concentrateWheatBran": MessageLookupByLibrary.simpleMessage("نخالة القمح"),
+    "concentrateWheatGrain": MessageLookupByLibrary.simpleMessage("حبوب القمح"),
+    "costLabel": MessageLookupByLibrary.simpleMessage("التكلفة\n(ERN)"),
+    "costPerKg": MessageLookupByLibrary.simpleMessage("التكلفة/كغم"),
+    "cowCharacteristics": MessageLookupByLibrary.simpleMessage("خصائص البقرة"),
+    "cowRequirements": MessageLookupByLibrary.simpleMessage("متطلبات البقرة"),
+    "cpIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك البروتين الخام",
+    ),
+    "cpIntakeLabelKgPerD": MessageLookupByLibrary.simpleMessage(
+      "استهلاك البروتين الخام\n(كغم/يوم)",
+    ),
+    "cpIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك البروتين الخام\n(%)",
+    ),
+    "crudeProteinLabel": MessageLookupByLibrary.simpleMessage("البروتين الخام"),
+    "dmIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك المادة الجافة",
+    ),
+    "dmIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك المادة الجافة\n(كغم/يوم)",
+    ),
+    "dryCowFeeding": MessageLookupByLibrary.simpleMessage(
+      "تغذية البقرة الجافة",
+    ),
+    "dryCowPeriod": MessageLookupByLibrary.simpleMessage(
+      "على وشك الولادة خلال 45 إلى 60 يوم",
+    ),
+    "dryMatterIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك المادة الجافة",
+    ),
+    "earlyLactationFeeding": MessageLookupByLibrary.simpleMessage(
+      "تغذية مرحلة الإدرار المبكرة",
+    ),
+    "earlyLactationPeriod": MessageLookupByLibrary.simpleMessage(
+      "مرحلة الإدرار المبكرة (14-100 يوم)",
+    ),
+    "feedFormula": MessageLookupByLibrary.simpleMessage("تركيبة العلف"),
+    "feedPricesAndAvailability": MessageLookupByLibrary.simpleMessage(
+      "أسعار الأعلاف وتوفرها",
+    ),
+    "feedingGuidelines": MessageLookupByLibrary.simpleMessage(
+      "إرشادات التغذية",
+    ),
+    "fodder": MessageLookupByLibrary.simpleMessage("علف"),
+    "fodderAlfafa": MessageLookupByLibrary.simpleMessage("البرسيم"),
+    "fodderBananaFruit": MessageLookupByLibrary.simpleMessage("ثمار الموز"),
+    "fodderBananaLeaves": MessageLookupByLibrary.simpleMessage("أوراق الموز"),
+    "fodderBananaStalks": MessageLookupByLibrary.simpleMessage("سيقان الموز"),
+    "fodderBarleyHay": MessageLookupByLibrary.simpleMessage("قش الشعير"),
+    "fodderBarleyStraw": MessageLookupByLibrary.simpleMessage("قش الشعير"),
+    "fodderCabbage": MessageLookupByLibrary.simpleMessage("ملفوف"),
+    "fodderElephantGrass": MessageLookupByLibrary.simpleMessage("عشب الفيل"),
+    "fodderElephantGrassHay": MessageLookupByLibrary.simpleMessage(
+      "قش عشب الفيل",
+    ),
+    "fodderEragrostisTefHay": MessageLookupByLibrary.simpleMessage("قش التف"),
+    "fodderGreenMaizeForage": MessageLookupByLibrary.simpleMessage(
+      "علف الذرة الخضراء",
+    ),
+    "fodderMaizeSilage": MessageLookupByLibrary.simpleMessage("سيلاج الذرة"),
+    "fodderPearlMilletFreshForage": MessageLookupByLibrary.simpleMessage(
+      "علف الدخن اللؤلؤي الطازج",
+    ),
+    "fodderPearlMilletStalk": MessageLookupByLibrary.simpleMessage(
+      "سيقان الدخن اللؤلؤي",
+    ),
+    "fodderSorghumBiProductResidue": MessageLookupByLibrary.simpleMessage(
+      "بقايا منتجات الذرة الرفيعة الثانوية",
+    ),
+    "fodderSorghumForage": MessageLookupByLibrary.simpleMessage(
+      "علف الذرة الرفيعة",
+    ),
+    "fodderSorghumStalk": MessageLookupByLibrary.simpleMessage(
+      "سيقان الذرة الرفيعة",
+    ),
+    "fodderWheatHay": MessageLookupByLibrary.simpleMessage("قش القمح"),
+    "fodderWheatStraw": MessageLookupByLibrary.simpleMessage("قش القمح"),
+    "fodderWildGrassFresh": MessageLookupByLibrary.simpleMessage(
+      "العشب البري الطازج",
+    ),
+    "fodderWildGrassHay": MessageLookupByLibrary.simpleMessage(
+      "قش العشب البري",
+    ),
+    "freshFeedIntake": MessageLookupByLibrary.simpleMessage(
+      "استهلاك العلف الطازج (كغم/يوم)",
+    ),
+    "freshFeedIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك العلف\nالطازج (كغم/يوم)",
+    ),
+    "ingredientLabel": MessageLookupByLibrary.simpleMessage("المكون"),
+    "kgPerDay": m1,
+    "kgValue": m2,
+    "lactationStage": MessageLookupByLibrary.simpleMessage("مرحلة الإدرار"),
+    "lactationStageDry": MessageLookupByLibrary.simpleMessage("جافة"),
+    "lactationStageEarlyLactation": MessageLookupByLibrary.simpleMessage(
+      "مرحلة الإدرار المبكرة",
+    ),
+    "lactationStageLateLactation": MessageLookupByLibrary.simpleMessage(
+      "مرحلة الإدرار المتأخرة",
+    ),
+    "lactationStageMidLactation": MessageLookupByLibrary.simpleMessage(
+      "مرحلة الإدرار المتوسطة",
+    ),
+    "lateLactationFeeding": MessageLookupByLibrary.simpleMessage(
+      "تغذية مرحلة الإدرار المتأخرة",
+    ),
+    "lateLactationPeriod": MessageLookupByLibrary.simpleMessage(
+      "مرحلة الإدرار المتأخرة (أكثر من 200 يوم)",
+    ),
+    "liveWeight": MessageLookupByLibrary.simpleMessage("الوزن الحي (كغم)"),
+    "meIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الطاقة المستقلبة",
+    ),
+    "meIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الطاقة المستقلبة\n(ميغاجول/يوم)",
+    ),
+    "midLactationFeeding": MessageLookupByLibrary.simpleMessage(
+      "تغذية مرحلة الإدرار المتوسطة",
+    ),
+    "midLactationPeriod": MessageLookupByLibrary.simpleMessage(
+      "مرحلة الإدرار المتوسطة (100 إلى 200 يوم)",
+    ),
+    "milkFat": MessageLookupByLibrary.simpleMessage("دهون الحليب (%)"),
+    "milkProtein": MessageLookupByLibrary.simpleMessage("بروتين الحليب (%)"),
+    "milkVolumePerDay": MessageLookupByLibrary.simpleMessage(
+      "كمية الحليب يومياً (كغم)",
+    ),
+    "milkYield": MessageLookupByLibrary.simpleMessage("إنتاج الحليب"),
+    "mjPerDayValue": m3,
+    "ndfIntakeLabel": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الألياف المحايدة",
+    ),
+    "ndfIntakeLabelKgPerD": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الألياف المحايدة\n(كغم/يوم)",
+    ),
+    "ndfIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الألياف المحايدة\n(%)",
+    ),
+    "ndfLabel": MessageLookupByLibrary.simpleMessage("الألياف المحايدة"),
+    "pIntakeLabel": MessageLookupByLibrary.simpleMessage("استهلاك الفوسفور"),
+    "pIntakeLabelKgPerD": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الفوسفور\n(كغم/يوم)",
+    ),
+    "pIntakeLabelWithUnit": MessageLookupByLibrary.simpleMessage(
+      "استهلاك الفوسفور\n(%)",
+    ),
+    "percentageValue": m4,
+    "period": MessageLookupByLibrary.simpleMessage("الفترة"),
+    "planFeed": MessageLookupByLibrary.simpleMessage("تخطيط التغذية"),
+    "pleaseAllFields": MessageLookupByLibrary.simpleMessage(
+      "يرجى ملء جميع الحقول.",
+    ),
+    "pleaseFillInAllFieldsBeforeViewingRequirements":
+        MessageLookupByLibrary.simpleMessage(
+          "يرجى ملء جميع الحقول قبل عرض المتطلبات.",
+        ),
+    "pregnancy": MessageLookupByLibrary.simpleMessage("الحمل (شهر)"),
+    "prices": MessageLookupByLibrary.simpleMessage("الأسعار"),
+    "selectRationIngredient": MessageLookupByLibrary.simpleMessage(
+      "اختر مكون الحصة الغذائية",
+    ),
+    "totals": MessageLookupByLibrary.simpleMessage("المجاميع"),
+    "viewCowRequirements": MessageLookupByLibrary.simpleMessage(
+      "عرض متطلبات البقرة",
+    ),
+  };
 }
